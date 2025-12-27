1c0755315ec73f9f1bf95deeab08a9bf1046ea4e
Arham Fareed <user@example.com>
2025-12-28 02:50:55 +0500

    Update assembly informational version and cache for TaskTracker.Tests

--- TaskTracker.Tests/obj/Debug/net8.0/TaskTracker.Tests.AssemblyInfo.cs
+++ TaskTracker.Tests/obj/Debug/net8.0/TaskTracker.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0893342148b196384e2f123194e04384ef9a3ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
